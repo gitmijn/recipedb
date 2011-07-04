7c875827f132b975a271d4a5e3e1dde95c450c30
Jeffrey T. Holden <jtholden@gmail.com>
2011-07-04 12:31:11 +0200

    Finished adding Ajax

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ActiveRecord::Base
-  validates_presence_of :title, :description
+#  validates_presence_of :title, :description
   has_many :steps, :dependent => :destroy
   accepts_nested_attributes_for :steps, :reject_if => lambda { |a| a[:instruction].blank? }, :allow_destroy => true
 end
